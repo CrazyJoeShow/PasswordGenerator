95ebdfc4e804d3f0e162933c82a3d712711c037e
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-03 21:55:16 -0400

    confirmsadded

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,22 @@ function generatePassword() {
 			'How many characters long would you like your password to be (between 8 and 128)?'
 		)
 	);
+	//is the input within the desired range 8-128
+	if (desiredPassLength >= 8 && desiredPassLength <= 128) {
+		var confirmedPassLength = desiredPassLength;
+	} else {
+		alert('Please enter a number between 8 and 128.');
+	}
+	// ask is uppercase allowed.
+	const upperCaseAllowed = confirm(
+		'Use lowercase letters in the new password?'
+	);
+	// ask if lowercase is allowed.
+	const lowerCaseAllowed = confirm(
+		'Use lowercase letters in the new password?'
+	);
+	//ask if numbers are llowed.
+	const numbersAllowed = confirm('Use numbers in the new password?');
+	// ask if special characters are allowed.
+	const symbosAllowed = confirm('Use special characters in the new password?');
 }
